ee97f76a55af72bce3f6c2764e4a0b4cccf8da99
이재석 <jslee01@miridih.com>
2026-02-05 15:33:30 +0900

    refactor(treesitter): remove unnecessary commented ensure_installed block

--- lua/ashrock/lazy/treesitter.lua
+++ lua/ashrock/lazy/treesitter.lua
@@ -3,10 +3,6 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      -- ensure_installed = {
-      --   "javascript", "typescript", "lua", "astro", "tsx", "json", "markdown"
-      -- },
-
       sync_install = false,
       auto_install = true,
 
